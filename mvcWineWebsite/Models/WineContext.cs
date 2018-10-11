f71fc1bf4b37d35c01582a64dea372fc2f27b22a
DerekPeacock <derek.peacock@yahoo.com>
2018-10-11 12:44:57 +0100

    Added a controller and views

---
+++ mvcWineWebsite/Models/WineContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvcWineWebsite.Models
+{
+    public class WineContext : DbContext
+    {
+        public WineContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Wine> Wines { get; set; }
+    }
+}
